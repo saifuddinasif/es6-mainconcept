0a551b94d6df02ef2a9b5778deb8e57a9b3602c4
My Name <mdarfinji45@gmail.com>
2022-09-01 10:30:23 +0800

    es6

---
+++ accessingObject.js
@@ -0,0 +1,26 @@
+
+
+const Person = {
+    name: 'abul',
+    age: 23,
+    contact: {
+        phone: '09432723',
+        email: "abul@gmail.com"
+    },
+    education: {
+        schoolName: "andorkilla high school",
+        subjects: [
+            { name: 'bangla', mark: 45 },
+            { name: 'english', mark: 64 },
+            { name: 'math', mark: 74 }
+        ]
+    }
+}
+const {name,age , education} = Person;
+// email
+// console.log(Person.contact.email);
+/* console.log(name)
+console.log(education.subjects[0].name)
+console.log(education.subjects[1].name)
+console.log(education.subjects[1].name)
+ */
